0d5f34ce0532de9a87ce9ca62ef3d656d497ac74
Jiayi <lijiayi0415@163.com>
2024-05-23 21:33:22 +1000

    first commit

---
+++ src/components/Pagination.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const Pagination = ({paginate, postsPerPage, currentPage, totalPosts} )=>{
+    const pageNumbers = [];
+
+    for(let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
+        pageNumbers.push(i);
+    }
+    
+    return (
+        <nav>
+            <ul className="pagination">
+            {pageNumbers.map((number) => (
+                <li key={number} className="page-item">
+                    <a 
+                    onClick={() => paginate(number)}
+                    href="!#" 
+                    className={`page-link ${
+                        currentPage === number ? "active-page" : ""
+                        }`}
+                    >
+                        {number}
+                    </a>
+                </li>
+                 ))}
+            </ul>
+    </nav>
+    );
+};
+
+export default Pagination;
